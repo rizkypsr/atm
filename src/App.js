d24e4ecce581f9d949b7cd98eae3eb69a4b0dc28
Rizky Pratama Syahrul Ramadhan <rizkypsrr@gmail.com>
2022-03-28 16:11:10 +0700

    change hompage for deployment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import { useContext, useMemo } from "react"
 import { useTranslation } from "react-i18next"
 import { Outlet } from "react-router-dom"
-import { AccountContext } from "./context/AccountContext"
 import { LocaleContext } from "./context/LocaleContext"
 
 const App = () => {
   const { i18n } = useTranslation()
-  const { account, setAccount } = useContext(AccountContext)
-  const value = useMemo(() => ({ account, setAccount }), [account])
 
   return (
     <>
       <LocaleContext.Provider value={i18n}>
-        <AccountContext.Provider value={value}>
-          <Outlet />
-        </AccountContext.Provider>
+        <Outlet />
       </LocaleContext.Provider>
     </>
   )
