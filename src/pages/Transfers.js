570ca78124909f30fa4957f604ff59472c5e98eb
Rizky Pratama Syahrul Ramadhan <rizkypsrr@gmail.com>
2022-03-30 17:53:44 +0700

    add validation input bank code and account number

--- src/pages/Transfers.js
+++ src/pages/Transfers.js
@@ -4,30 +4,47 @@ import { Card } from "../components/Card"
 import { TextInput } from "../components/TextInput"
 import { AccountContext } from "../context/AccountContext"
 import { useNavigate } from "react-router-dom"
+import { Alert } from "../components/Alert"
+import { LocaleContext } from "../context/LocaleContext"
 
 export const Transfers = () => {
   const navigate = useNavigate()
   const { setAccount } = useContext(AccountContext)
-  const [accountNumber, setAccountNumber] = useState()
+  const { t } = useContext(LocaleContext)
+  const [accountNumber, setAccountNumber] = useState("")
+  const [bankCode, setBankCode] = useState("")
+  const [validationErr, setValidationErr] = useState(false)
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      setAccount((acc) => ({
-        ...acc,
-        accountNumber,
-      }))
+      if (checkInput) {
+        setAccount((acc) => ({
+          ...acc,
+          accountNumber,
+        }))
 
-      navigate("/transfers-amount")
+        navigate("/transfers-amount")
+      } else {
+        setValidationErr(true)
+      }
     }
   }
+
+  const checkInput = bankCode !== '' && accountNumber != ''
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-6 space-y-8">
       <Title name="inputTransferDestination" />
 
+      {validationErr && <Alert title={t("emptyInput")} />}
+
       <Card>
         <div className="flex items-center space-x-3">
-          <TextInput className="w-20" onKeyDown={handleKeyDown} />
+          <TextInput
+            className="w-20"
+            onKeyDown={handleKeyDown}
+            onChange={(e) => setBankCode(e.target.value)}
+          />
           <span>+</span>
           <TextInput
             className="w-full"
